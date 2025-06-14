2e9409e1c7919c632dcafcd29ce7778497b4d3a8
Gabriele Basigli <84378604+Basigli@users.noreply.github.com>
2025-06-14 17:43:45 +0200

    (feat) coordinator: base structure

---
+++ ConcurrentBoidActors/src/main/java/it/unibo/message/BoidCoordinator.java
@@ -0,0 +1,42 @@
+package it.unibo.message;
+
+import akka.actor.typed.Behavior;
+import akka.actor.typed.javadsl.AbstractBehavior;
+import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.Receive;
+
+public class BoidCoordinator extends AbstractBehavior<BoidMessage> {
+    private BoidCoordinator(ActorContext<BoidMessage> context) {
+        super(context);
+    }
+
+    @Override
+    public Receive<BoidMessage> createReceive() {
+        return newReceiveBuilder()
+                .onMessage(Start.class, this::onStart)
+                .onMessage(VelocityComputed.class, this::onVelocityComputed)
+                .onMessage(VelocityUpdated.class, this::onVelocityUpdated)
+                .onMessage(PositionUpdated.class, this::onPositionUpdated)
+                .build();
+    }
+
+
+    private Behavior<BoidMessage> onStart(Start msg) {
+        return this;
+    }
+
+    private Behavior<BoidMessage> onVelocityComputed(VelocityComputed msg) {
+        return this;
+    }
+
+    private Behavior<BoidMessage> onVelocityUpdated(VelocityUpdated msg) {
+        return this;
+    }
+
+    private Behavior<BoidMessage> onPositionUpdated(PositionUpdated msg) {
+        return this;
+    }
+
+
+
+}
