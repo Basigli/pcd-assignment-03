9610b69c62172fad48bad09a22b9a90b96143bb6
Gabriele Basigli <84378604+Basigli@users.noreply.github.com>
2025-07-27 14:41:12 +0200

    (chore)boids: removed useless CoordinatorStatus

--- ConcurrentBoidActors/src/main/java/it/unibo/message/CoordinatorStatus.java
+++ ConcurrentBoidActors/src/main/java/it/unibo/message/CoordinatorStatus.java
@@ -3,7 +3,5 @@ package it.unibo.message;
 public enum CoordinatorStatus {
     PAUSED,
     RESETTING,
-    COMPUTING_VELOCITY,
-    UPDATING_VELOCITY,
-    UPDATING_POSITION
+    COMPUTING_VELOCITY
 }
